f426c85929917562caf370903269fc272e53240d
Hy Hyeon Chan <lotteh77@naver.com>
2021-12-20 00:15:13 +0900

    day_1

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text,View, SafeAreaView } from "react-native";
+
+const App = () => {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Hello React!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native'; //JS 문법 
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
